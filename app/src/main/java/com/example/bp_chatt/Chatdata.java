9a0414fbaba9f8b5ba70a27a99d344b2a07aea06
wnsgk42 <57088799+wnsgk42@users.noreply.github.com>
2020-03-05 17:40:31 +0900

    time

--- app/src/main/java/com/example/bp_chatt/Chatdata.java
+++ app/src/main/java/com/example/bp_chatt/Chatdata.java
@@ -1,59 +1,36 @@
 package com.example.bp_chatt;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
+import androidx.recyclerview.widget.RecyclerView;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(TextView v) {
-            super(v);
-            textView = v;
-        }
+public class Chatdata {
+    private String nick;
+    private String msg;
+
+    public Chatdata(){
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public String getNick() {
+        return nick;
     }
 
-    // Create new views (invoked by the layout manager)
-    @Override
-    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                     int viewType) {
-        // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
-            ...
-        MyViewHolder vh = new MyViewHolder(v);
-        return vh;
+    public void setNick(String nick) {
+        this.nick = nick;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
-
+    public String getMsg() {
+        return msg;
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataset.length;
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Chatdata (String nick, String msg){
+        this.nick=nick;
+        this.msg=msg;
     }
 }
--- app/src/main/java/com/example/bp_chatt/MainActivity.java
+++ app/src/main/java/com/example/bp_chatt/MainActivity.java
@@ -1,14 +1,105 @@
 package com.example.bp_chatt;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    Button Button_send;
+    EditText EditText_msg;
+
+    private RecyclerView recyclerView; // RecyclerView 클래스를 사용키 위한 참조변수
+    private RecyclerView.Adapter mAdapter; // RecyclerView 클래스를 사용키 위한 참조변수
+    private RecyclerView.LayoutManager layoutManager;// RecyclerView 클래스를 사용키 위한 참조변수
+    private ArrayList<Chatdata> myDataset;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private String nick="data_nick";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview_chat);//RecyclerView 참조
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference();
+
+        Button_send = findViewById(R.id.Button_send);
+        EditText_msg = findViewById(R.id.EditText_msg);
+
+        Button_send.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Chatdata chatdata= new Chatdata();
+                chatdata.setMsg(EditText_msg.getText().toString());
+                chatdata.setNick(nick);
+                myRef.push().setValue(chatdata);
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Chatdata chat =dataSnapshot.getValue(Chatdata.class);
+                ((MyAdapter)mAdapter).addChat(chat);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);//크기 고정
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);//layout manager 인스턴스생성 및 context 생성자 호춯
+        recyclerView.setLayoutManager(layoutManager);
+
+        myDataset = new ArrayList<>();
+        myDataset.add(new Chatdata("wrp","hi"));//list에 임의의 값 지정
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
     }
+    // ...
 }
